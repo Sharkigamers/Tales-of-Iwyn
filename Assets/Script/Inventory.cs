18ba85b64063d3b68bce30d11714f13f44d410f6
Eben <clement.humblot@epitech.eu>
2021-11-16 07:25:15 +0900

    [ADD] item pickup and gamemanager inventory singleton

---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+
+    public delegate void OnItemChanged();
+
+    public OnItemChanged onItemChangedCallback;
+
+    public static Inventory instance;
+    
+    //TODO SPACE AND STACK
+    
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+        }
+        instance = this;
+    }
+
+    #endregion
+    
+    public List<Item> items = new List<Item>();
+
+    public void Add(Item item)
+    {
+        items.Add(item);
+
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+    
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+        
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
