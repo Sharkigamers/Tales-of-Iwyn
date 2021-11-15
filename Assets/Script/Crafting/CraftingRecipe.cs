78d1dbdfbbf5ec2f3d8ce62c7b60aa26ec531c67
Danny <gabriel.danjon@epitech.eu>
2021-11-15 20:35:30 +0900

    [ADD]: settings menu

---
+++ Assets/Script/Crafting/CraftingRecipe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct ItemAmount
+{
+    public Item item;
+    [Range(1, 999)]
+    public int amount;
+}
+
+[CreateAssetMenu]
+public class CraftingRecipe : ScriptableObject
+{
+    public List<ItemAmount> materials;
+    public List<ItemAmount> results;
+
+    public bool CanCraft(IItemContainer itemContainer) {
+        foreach (ItemAmount itemAmount in materials)
+            if (itemContainer.ItemCount(itemAmount.item) < itemAmount.amount)
+                return false;
+        return true;
+    }
+
+    public void Craft(IItemContainer itemContainer) {
+        if (CanCraft(itemContainer)) {
+            foreach (ItemAmount itemAmount in materials)
+                for (int i = 0; i < itemAmount.amount; ++i)
+                    itemContainer.RemoveItem(itemAmount.item);
+            foreach (ItemAmount itemAmount in materials)
+                for (int i = 0; i < itemAmount.amount; ++i)
+                    itemContainer.AddItem(itemAmount.item);
+        }
+    }
+}
---
+++ Assets/Script/Crafting/ItemContainer.cs
@@ -0,0 +1,8 @@
+﻿public interface IItemContainer
+{
+    int ItemCount(Item item);
+    bool ContainsItem(Item item);
+    bool RemoveItem(Item item);
+    bool AddItem(Item item);
+    bool IsFull();
+}
