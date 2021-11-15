894d79a83531931e76dfff9c0a35dd2da1114f16
jsatge <jeannie.satge@epitech.eu>
2021-11-15 21:08:44 +0900

    adding healing spell and fire spell

---
+++ Assets/Script/Player/SpellManagement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellManagement : MonoBehaviour
+{
+    public GameObject FireBallPrefab;
+    public ParticleSystem Heal;
+    public float FirePower;
+
+    private Transform playerPosition;
+
+    void Awake() {
+        playerPosition =  GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Heal.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            CastFireBall();
+        }        
+    }
+
+    private void CastFireBall() {
+        Vector3 fireBallPos = new Vector3(playerPosition.position.x, (playerPosition.position.y + 1), playerPosition.position.z);
+        GameObject FireBall = Instantiate(FireBallPrefab, fireBallPos, Quaternion.identity);
+        FireBall.GetComponent<Rigidbody>().velocity = playerPosition.TransformDirection(Vector3.forward * FirePower);
+    }
+}
