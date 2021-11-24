c7fb447e05bf081afaa3b02ecb038c0ee4b4f02a
Alkeon <lucas.arsene@epitech.eu>
2021-11-25 00:51:12 +0900

    feat: linking hud and player stats

--- Assets/Script/Player/BarManager.cs
+++ Assets/Script/Player/BarManager.cs
@@ -5,14 +5,12 @@ using UnityEngine.UI;
 
 public class BarManager : MonoBehaviour
 {
-    public float health = 100;
-    public float mana = 100;
     public Image healthBar;
     public Image manaBar;
  
     void Update()
     {
-        healthBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * 2);
-        manaBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, mana * 2);        
+        healthBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, PlayerPrefs.GetInt("Health") * 2);
+        manaBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, PlayerPrefs.GetInt("Mana") * 2);        
     }
 }
