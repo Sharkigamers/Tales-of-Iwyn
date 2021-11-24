4e07118c2f77541ba6c48faee110d7c83d117c13
jsatge <jeannie.satge@epitech.eu>
2021-11-24 23:47:18 +0900

    feat: add hud

---
+++ Assets/Script/Player/BarManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarManager : MonoBehaviour
+{
+    public float health = 100;
+    public float mana = 100;
+    public Image healthBar;
+    public Image manaBar;
+ 
+    void Update()
+    {
+        healthBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * 2);
+        manaBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, mana * 2);        
+    }
+}
