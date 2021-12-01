57186277b84773147affb93cc05ea0b5d3db60a6
jsatge <jeannie.satge@epitech.eu>
2021-12-01 14:43:46 +0900

    beginning map

---
+++ Assets/Script/Map.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Map : MonoBehaviour
+{
+    [SerializeField] GameObject mapDisplay;
+    private bool isActive = false;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.M)) {
+            isActive = !isActive;            
+            if (!isActive) {
+                mapDisplay.SetActive(false);
+                Time.timeScale = 1f;
+            } else {
+            mapDisplay.SetActive(true);
+            Time.timeScale = 0f;
+            }
+        }
+    }
+}
