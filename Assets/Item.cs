0f6680606bf7ff463ef265264f48d9c097be5d9f
Eben <clement.humblot@epitech.eu>
2021-12-09 06:30:53 +0900

    Inventory UI

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -12,8 +12,22 @@ public class Item : ScriptableObject
         }
     }
 	new public string ItemName = "New Item";
-	public Sprite Icon = null;
+	public Sprite icon = null;
 
 	[Range(1,999)]
 	public int MaximumStacks = 1;
+	
+	// Called when the item is pressed in the inventory
+	public virtual void Use ()
+	{
+		// Use the item
+		// Something might happen
+
+		Debug.Log("Using " + name);
+	}
+
+	public void RemoveFromInventory ()
+	{
+		Inventory.instance.Remove(this);
+	}
 }
