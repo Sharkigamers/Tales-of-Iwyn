b5fe3aa21c0ea716051bc4a477e5b1aa8cb17730
Eben <clement.humblot@epitech.eu>
2021-12-16 08:03:50 +0900

    Final build, need clean

---
+++ Assets/rotationIcon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationIcon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public float lockPos;
+
+    void Update()
+    {
+        // Locks the rotation.
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, transform.rotation.eulerAngles.x);
+    }
+}
